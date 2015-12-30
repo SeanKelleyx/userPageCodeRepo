ce0d11a140c4c1b4bc4542d21b8ab6ffdac78c49
Sean Kelley <s.kelley27@gmail.com>
2015-12-29 21:04:27 -0600

    fix touchscreen detection

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,14 @@
-var isTouchDevice = 'ontouchstart' in document.documentElement;
+var hasTouch;
+window.addEventListener('touchstart', function setHasTouch () {
+    touchDevice();
+    window.removeEventListener('touchstart', setHasTouch);
+}, false);
 
 $("#tools a").click(function(event){
 	event.preventDefault();
 });
 
-if(isTouchDevice){
+function touchDevice(){
 	$(".portfolio-box .portfolio-box-caption")
 	.css('background', 'rgba(209, 142, 24, 0.6) none repeat scroll 0 0')
 	.css('opacity', 1);
