26a42de12b2a84d4cbd7eb1c50f1be2728308a18
Sean Kelley <s.kelley27@gmail.com>
2015-12-29 19:53:49 -0600

    fixing styles for touch (mobile) devices that dont have the hove for the tools section

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,11 @@
+var isTouchDevice = 'ontouchstart' in document.documentElement;
+
 $("#tools a").click(function(event){
 	event.preventDefault();
 });
+
+if(isTouchDevice){
+	$(".portfolio-box .portfolio-box-caption")
+	.css('background', 'rgba(209, 142, 24, 0.6) none repeat scroll 0 0')
+	.css('opacity', 1);
+}
