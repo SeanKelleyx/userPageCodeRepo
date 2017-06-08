bfa3c9e2d7184e7148e0a23d2658f7562a4f009a
Sean Kelley <s.kelley27@gmail.com>
2017-06-07 20:57:21 -0500

    adding build html from template

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
     header = require('gulp-header'),
     cleanCSS = require('gulp-clean-css'),
     rename = require("gulp-rename"),
+    replace = require('gulp-replace'),
     uglify = require('gulp-uglify'),
     concat = require('gulp-concat'),
     maps = require('gulp-sourcemaps'),
@@ -27,22 +28,57 @@ gulp.task('less', function() {
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
+});
+
+// Concatenate CSS
+gulp.task('concat-css', ['less'], function(){
+	return gulp.src(['vendor/bootstrap/css/bootstrap.min.css',
+		'vendor/font-awesome/css/font-awesome.min.css',
+		'https://fonts.googleapis.com/css?family=Montserrat:400,700',
+		'https://fonts.googleapis.com/css?family=Kaushan+Script',
+		'https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic',
+		'https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700',
+		'css/agency.css'])
+		.pipe(maps.init())
+		.pipe(concat('main.css'))
+		.pipe(maps.write('./'))
+    	.pipe(gulp.dest("css"))
+    	.pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 // Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/agency.css')
+gulp.task('minify-css', ['concat-css'], function() {
+    return gulp.src('css/main.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('css'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
+});
+
+// Concatenate JS
+gulp.task('concat-js', function(){
+	return gulp.src(['vendor/jquery/jquery.min.js',
+		'vendor/bootstrap/js/bootstrap.min.js',
+		'https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js',
+		'js/jqBootstrapValidation.js',
+		'js/contact_me.js',
+		'js/agency.js'])
+		.pipe(maps.init())
+		.pipe(concat('main.js'))
+		.pipe(maps.write('./'))
+    	.pipe(gulp.dest("js"))
+    	.pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 // Minify JS
-gulp.task('minify-js', function() {
+gulp.task('minify-js', ['concat-js'], function() {
     return gulp.src('js/agency.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
@@ -50,7 +86,19 @@ gulp.task('minify-js', function() {
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
+});
+
+// Create html
+gulp.task('dev-html', function(){
+	return gulp.src('index.template')
+		.pipe(replace('{{STYLE}}', 'css/main.css'))
+		.pipe(replace('{{SCRIPT}}', 'js/main.js'))
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest('./'))
+		.pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 // Copy vendor libraries from /node_modules into /vendor
@@ -69,7 +117,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('vendor/font-awesome'))
+        .pipe(gulp.dest('vendor/font-awesome'));
 })
 
 // Run everything
@@ -81,27 +129,16 @@ gulp.task('browserSync', function() {
         server: {
             baseDir: ''
         },
-    })
+    });
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js', 'dev-html'], function() {
     gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
+    gulp.watch('index.template', ['dev-html']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
-});
-
-// Compiles SCSS files from /scss into /css
-// NOTE: This theme uses LESS by default. To swtich to SCSS you will need to update this gulpfile by changing the 'less' tasks to run 'sass'!
-gulp.task('sass', function() {
-    return gulp.src('scss/agency.scss')
-        .pipe(sass())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
 });
