23aac994348fc8098a566a3c43a4c1348e3b7e2f
Sean Kelley <s.kelley27@gmail.com>
2018-05-10 13:03:44 -0600

    use new mailing service to ensure deliverey regardless of system configuration

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,14 +1,17 @@
 <?php
+require '../vendor/autoload.php';
+use \Mailjet\Resources;
+$mj = new \Mailjet\Client('5f0611959399da35b02d2176ea59abd2', '16be4ef105f563a07895e7d2ddd717a9');
+
 // Check for empty fields
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
    empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
    echo "No arguments Provided!";
    return false;
-   }
+}
    
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
@@ -19,8 +22,20 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 's.kelley27@gmail.com';
 $email_subject = "Portfolio Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@seankelley.tech\n"; 
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+$email_body_html = '<h4>You have received a new message from you website contact form.</h2><p>Here are the details:</p><ul><li>Name:  '.$name.'</li><li>Email: '.$email_address.'</li><li>Phone: '.$phone.'</li></ul><p><u>Message:</u> '.$message.'</p>'; 
+$body = [
+    'FromEmail' => "noreply@seankelley.tech",
+    'FromName' => "Resume Website",
+    'Subject' => "Portfolio Website Contact Form: ",
+    'Text-part' => $email_body,
+    'Html-part' => $email_bodyi_html,
+    'Recipients' => [
+        [
+            'Email' => "s.kelley27@gmail.com"
+        ]
+    ]
+];
+
+$response = $mj->post(Resources::$Email, ['body' => $body]);
+return $response->success()  
 ?>
